49ba11225e462b3cca133ffccb872040149bcc17
dhirajyadav91 <dhirajdhoni97@gmail.com>
2025-11-20 10:16:15 -0800

    p

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:8080', // your backend API URL
+      '/api': 'http://localhost:8080', // local dev backend
     },
   },
-});
+
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT,        
+    allowedHosts: ["ichat-f.onrender.com"],  // ← ADD THIS
+  },
+})
